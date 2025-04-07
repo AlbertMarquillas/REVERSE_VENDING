7e0d272fb40330e06d9a50106a44909098195d45
Albert <73698387+AlbertMarquillas@users.noreply.github.com>
2025-04-07 19:27:13 +0200

    Update sensors.h

--- ESP32/sensors.h
+++ ESP32/sensors.h
@@ -7,13 +7,10 @@ class Sensors {
 public:
     Sensors() {};
     void setup_sensors();
-    float read_temperature();
-    float read_humidity();
     float read_ultrasonic();
     float read_load_cell();
     bool read_inductive_sensor();
     int read_light_sensor();
-    bool read_cut_sensor();
 };
 
 #endif
